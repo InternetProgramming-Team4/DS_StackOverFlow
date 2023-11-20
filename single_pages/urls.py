abe5c382e0f6d171e5026ee9bbd12784e0d3583a
killerwhale915 <kimjiwoo9622@naver.com>
2023-11-20 17:57:41 +0900

    Update

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('MyPage/', views.user),
-    path('', views.main),
-    path('login/', views.login),
-    path('signup/', views.signup),
+    path('MyPage/', views.user,name='MyPage'),
+    path('', views.main,name='main'),
+    path('login/', views.login,name='login'),
+    path('signup/', views.signup,name='signup'),
 ]
