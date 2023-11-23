8d031da85a43aba454357bef102af00860d5d914
Narae Gwon <rnjs021031@gmail.com>
2023-11-23 14:33:07 +0900

    Post 모델 구현 및 전공별 리스트 출력 기능 구현

--- post/admin.py
+++ post/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Major
 
 # Register your models here.
 
 admin.site.register(Post)
+
+
+class MajorAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
+
+admin.site.register(Major, MajorAdmin)
