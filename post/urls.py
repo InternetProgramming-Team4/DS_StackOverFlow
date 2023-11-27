d2181d5fc0cd294a5320204381b4fb9465568338
Narae Gwon <rnjs021031@gmail.com>
2023-11-28 00:03:19 +0900

    url 수정

--- post/urls.py
+++ post/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.nomajorlist),
     path('<str:slug>/', views.major_page, name='Qlist'),
     path('<str:slug>/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
-    path('<str:slug>/<int:pk>/upvote/', views.UpvotePostView.as_view(), name='upvote_post'),
-    path('<str:slug>/<int:pk>/downvote/', views.DownvotePostView.as_view(), name='downvote_post'),
+    path('<str:slug>/<int:pk>/upvote_post/', views.UpvotePostView.as_view(), name='upvote_post'),
+    path('<str:slug>/<int:pk>/downvote_post/', views.DownvotePostView.as_view(), name='downvote_post'),
 ]
