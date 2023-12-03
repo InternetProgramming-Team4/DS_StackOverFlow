30f42801a91e2ca2f981707c1c2d8738e5c3fd53
SeungHee <hanseha17@gmail.com>
2023-12-03 16:33:55 +0900

    m

--- post/urls.py
+++ post/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name='post'
 urlpatterns = [
     path('', views.nomajorlist),
+    path('search/', views.PostSearchView.as_view(), name='post_searched'),
     path('create_post/', views.PostCreate.as_view(), name='create_post'),
     path('delete_post/<int:pk>', views.PostDelete.as_view(), name='delete_post'),
     path('update_post/<int:pk>', views.PostUpdate.as_view(), name='update_post'),
@@ -17,5 +18,5 @@ urlpatterns = [
     path('<str:slug>/<int:pk>/new_comment/', views.new_comment),
     path('<str:slug>/<int:pk>/upvote_comment/<int:comment_pk>', views.UpvoteCommentView.as_view(), name='upvote_comment'),
     path('<str:slug>/<int:pk>/downvote_comment//<int:comment_pk>', views.DownvoteCommentView.as_view(), name='downvote_comment'),
-    path('/search/', views.PostSearchView.as_view(), name='post_searched'),
+    path('search/', views.PostSearchView.as_view(), name='post_searched'),
 ]
